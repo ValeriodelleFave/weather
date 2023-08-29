b0673ade0b053e5650a4cfb48b6b7ff577a8a457
Valerio delle Fave <48824674+ValeriodelleFave@users.noreply.github.com>
2023-08-29 22:17:22 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,35 @@
-const API = "https://api.open-meteo.com/v1/forecast?hourly=temperature_2m";
+const API = "https://api.open-meteo.com/v1/forecast?hourly=temperature_2m,precipitation_probability,weathercode";
 const searchAPI = "https://geocoding-api.open-meteo.com/v1/search";
+const WMO = {
+    0: "Clear sky",
+    1: "Mainly clear",
+    2: "partly cloudy",
+    3: "and overcast",
+    45: "Fog",
+    48: "depositing rime fog",
+    51: "Drizzle Light",
+    53: "Drizzle moderate",
+    55:	"Drizzle: dense intensity",
+    56: "Freezing Drizzle: Light intensity", 
+    57: "Freezing Drizzle dense intensity",
+    61: "Rain: Slight intensity", 
+    63: "Rain: moderate intensity", 
+    65: "Rain: heavy intensity",	
+    66: "Freezing Rain: Light intensity", 
+    67: "Freezing Rain: heavy intensity",
+    71: "Snow fall: Slight intensity", 
+    73: "Snow fall: moderate intensity", 
+    75: "Snow fall: heavy intensity",
+    77:	"Snow grains",
+    80: "Rain showers: Slight", 
+    81: "Rain showers: moderate", 
+    82: "Rain showers: violent", 
+    85: "Snow showers slight", 
+    86: "Snow showers heavy", 
+    95: "Thunderstorm: Slight or moderate",
+    96:	"Thunderstorm with slight hail",
+    99:	"Thunderstorm with heavy hail"
+};
 
 async function search() {
     resetData("search-container", "weather-title", "days-container", "weather-container");
@@ -35,43 +65,51 @@ async function getData() {
     const days = [];
     const hours = raw.hourly;
     for (let index = 0, mapIndex = 0; index < hours.time.length; index++) {
+        const object = new Object();
+        for (const key in hours) {
+            object[key] = hours[key][index];
+        }
         if (index == 0) {
-            const map = new Map();
-            map.set(hours.time[index], hours.temperature_2m[index]);
-            days.push(map);
+            const day = [];
+            day.push(object);
+            days.push(day);
         } else if (index % 24 == 0) {
-            const map = new Map();
-            map.set(hours.time[index], hours.temperature_2m[index]);
-            days.push(map);
+            const day = [];
+            day.push(object);
+            days.push(day);
             mapIndex++;
         } else {
-            days[mapIndex].set(hours.time[index], hours.temperature_2m[index]);
+            days[mapIndex].push(object);
         }
     }
 
     resetData("days-container");
     for (const [index, day] of days.entries()) {
         const button = document.createElement("button");
-        button.textContent = getFormattedOptionDate(new Date(day.entries().next().value[0]));
-        button.onclick = () => { buildHourComponents(days, index) };
+        button.textContent = getFormattedOptionDate(new Date(day[0].time));
+        button.onclick = () => { buildHourComponents(day) };
         document.getElementById("days-container").appendChild(button);
     }
 }
 
-function buildHourComponents(days, index) {
+function buildHourComponents(hours, index) {
     resetData("weather-container");
-    const day = days[index];
-    day.forEach((temperature, hour) => {
+    for (const hour of hours) {
         const span = document.createElement("p");
-        span.className = "hour";
-        if (temperature <= 18) {
-            span.className += " cold";
-        } else if (temperature > 30) {
-            span.className += " hot";
+        for (const key in hour) {
+            span.className = "hour";
+            if (key == "weathercode") {
+                span.innerHTML += `${WMO[hour[key]]} `;
+            } else if (key == "time") {
+                span.innerHTML += `${getFormattedHour(new Date(hour[key]))} `;
+            } else if (key == "temperature_2m") {
+                span.innerHTML += `${hour[key]} °C`;
+            } else {
+                span.innerHTML += `${hour[key]} `;
+            }
         }
-        span.innerHTML = `${getFormattedHour(new Date(hour))} : ${temperature + "°"}`;
         document.getElementById("weather-container").appendChild(span);
-    })
+    }
 }
 
 /**
